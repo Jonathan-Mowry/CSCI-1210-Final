c195967daae15f83d4d676da8ffcdf3435e34115
Jonathan-Mowry <joatman16@yahoo.com>
2021-04-05 17:48:32 -0400

    Update random_pic_gen.js
    Removed unused checks in savePic(). Added comments. Updated check for total number of pictures used to 10 instead of 9.

--- js/random_pic_gen.js
+++ js/random_pic_gen.js
@@ -1,21 +1,24 @@
+//Array that holds the file name for a given picture.
 var picNameArr = ["Dragon_eye.jpg", "elephant.jpeg", "flaming_skull.jpg", "hand_and_rose.jpg", "jellyfish.jpg", "many_tentacles.jpg", "skeleton_face_2.jpeg", "skeleton_face.jpg", "tentacle.jpg", "oogieboogie.jpg"];
+//Array that holds the alt for each picture.
 var picAltArr = ["Dragon's Eye", "Elephant looking up the the sky", "Flaming Skull", "Skeleton hand holding a rose", "A jellyfish", "A horde of tentacles reaching towards the moon" , "A skeleton's face", "A skeleton's face", "A single tentacle", "oogie boogie from the nightmare before Christmas"];
+//total number of files
  arrCount = 10;
  randDriver();
-
+//returns a number between 0 and 9 (0 indexing)
 function randNum() {
     return Math.floor(Math.random() * arrCount);
 }
-
+//Saves the given picture number to sessionStorage.
 function savePic(int) {
     var ss = sessionStorage.getItem("picsUsed");
-    if (ss == null || ss == "" || ss == "null")
+    if (ss == null)
         ss = int;
     else
         ss += "|" + int;
     sessionStorage.setItem("picsUsed", ss);
 }
-
+//Retrieve an array of currently used picture numbers
 function getPicInts() {
     var ss = sessionStorage.getItem("picsUsed");
     if (ss != null)
@@ -23,7 +26,7 @@ function getPicInts() {
     ss = [];
     return ss;
 }
-
+//Splits the string on |
 function split(string, charStr) {
     var strArray = string.split('');
     var stringBuilt = "";
@@ -46,26 +49,26 @@ function split(string, charStr) {
     }
     return strArrayRebuilt;
 }
-
+//Main driver function, checks
 function randDriver() {
     var arrOfPics = getPicInts();
-    if (arrOfPics.length >= 9) {
+    if (arrOfPics.length >= 10) { //checks if all pics have been used.
         sessionStorage.clear();
         arrOfPics = [];
     }
     var num = randNum()
     var temp = 0;
     if (arrOfPics.length != 0) {
-        for (var i = 0; i < arrOfPics.length; i++) {
+        for (var i = 0; i < arrOfPics.length; i++) { //loops to check if current random number has been used.
             temp = arrOfPics[i];
-            if (num == temp) {
+            if (num == temp) { //number found
                 i = -1;
                 num = randNum();
             }
         }
     }
     savePic(num);
-    var img = document.getElementById("rand-img");
+    var img = document.getElementById("rand-img"); //update picture info
     img.src = "../resources/gallery_images/" + picNameArr[num];
     img.alt = picAltArr[num];
 }
