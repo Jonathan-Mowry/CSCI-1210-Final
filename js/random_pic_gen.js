f50f04ea783e7fa27ec253511b5792bde0ec45ec
Jonathan-Mowry <joatman16@yahoo.com>
2021-04-05 14:01:10 -0400

    Updated files
    Added img and button to index.html for random picture. Started random_pic_gen.js for logic of random picture

---
+++ js/random_pic_gen.js
@@ -0,0 +1,7 @@
+var picNameArr = ["Dragon_eye.jpg", "elephant.jpg", "flaming_skull.jpg", "hand_and_rose.jpg", "jellyfish.jpg", "many_tentacles.jpg", "skeleton_face_2.jpg", "skeleton_face.jpg", "tentacle.jpg"];
+var arrCount = 9;
+
+function randNum(arrCount) {
+    return Math.floor(Math.random() * arrCount);
+}
+
