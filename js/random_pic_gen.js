137fbc80224f05daddfb66f11ecebb80c23078e3
Jonathan-Mowry <joatman16@yahoo.com>
2021-04-05 17:40:29 -0400

    Updated files
    Finished the logic for random_pic_gen.js. Finished the html for the random pic in index.html. Added the picture oogieboogie.jpg to the gallery and optimized it. Updated gallery.html to include the new image. Changed the design of images in gallery.html to use figures and figcaptions. Figcaption will need to be updated once a summary of each picture is obtained.

--- js/random_pic_gen.js
+++ js/random_pic_gen.js
@@ -1,7 +1,71 @@
-var picNameArr = ["Dragon_eye.jpg", "elephant.jpg", "flaming_skull.jpg", "hand_and_rose.jpg", "jellyfish.jpg", "many_tentacles.jpg", "skeleton_face_2.jpg", "skeleton_face.jpg", "tentacle.jpg"];
-var arrCount = 9;
+var picNameArr = ["Dragon_eye.jpg", "elephant.jpeg", "flaming_skull.jpg", "hand_and_rose.jpg", "jellyfish.jpg", "many_tentacles.jpg", "skeleton_face_2.jpeg", "skeleton_face.jpg", "tentacle.jpg", "oogieboogie.jpg"];
+var picAltArr = ["Dragon's Eye", "Elephant looking up the the sky", "Flaming Skull", "Skeleton hand holding a rose", "A jellyfish", "A horde of tentacles reaching towards the moon" , "A skeleton's face", "A skeleton's face", "A single tentacle", "oogie boogie from the nightmare before Christmas"];
+ arrCount = 10;
+ randDriver();
 
-function randNum(arrCount) {
+function randNum() {
     return Math.floor(Math.random() * arrCount);
 }
 
+function savePic(int) {
+    var ss = sessionStorage.getItem("picsUsed");
+    if (ss == null || ss == "" || ss == "null")
+        ss = int;
+    else
+        ss += "|" + int;
+    sessionStorage.setItem("picsUsed", ss);
+}
+
+function getPicInts() {
+    var ss = sessionStorage.getItem("picsUsed");
+    if (ss != null)
+        return split(ss, "|");
+    ss = [];
+    return ss;
+}
+
+function split(string, charStr) {
+    var strArray = string.split('');
+    var stringBuilt = "";
+    var strArrayRebuilt = [];
+    for (var i = 0; i < strArray.length; i++) {
+        var temp = strArray[i];
+        if (i + 1 == strArray.length) {
+            stringBuilt += temp;
+            strArrayRebuilt.push(stringBuilt);
+            stringBuilt = "";
+        }
+        else if (temp != charStr)
+            stringBuilt += temp;
+        else {
+            strArrayRebuilt.push(stringBuilt);
+            stringBuilt = "";
+            if (strArray[i + 1] == ' ')
+                i++;
+        }
+    }
+    return strArrayRebuilt;
+}
+
+function randDriver() {
+    var arrOfPics = getPicInts();
+    if (arrOfPics.length >= 9) {
+        sessionStorage.clear();
+        arrOfPics = [];
+    }
+    var num = randNum()
+    var temp = 0;
+    if (arrOfPics.length != 0) {
+        for (var i = 0; i < arrOfPics.length; i++) {
+            temp = arrOfPics[i];
+            if (num == temp) {
+                i = -1;
+                num = randNum();
+            }
+        }
+    }
+    savePic(num);
+    var img = document.getElementById("rand-img");
+    img.src = "../resources/gallery_images/" + picNameArr[num];
+    img.alt = picAltArr[num];
+}
